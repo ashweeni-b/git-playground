f75506e9423cb2999dddcbbc7a384af479da415a
ashweeni-bharde <bhardeashweeni05@gmail.com>
2025-08-04 16:28:18 +0530

    2nd commit: modified ex1 and ex2 python files

---
+++ ex1-forloop.py
@@ -0,0 +1,6 @@
+i = 5
+
+for i in range(i):
+    print("Hello")
+
+print("1st modification for 2nd commit")
---
+++ ex2-dfunction.py
@@ -0,0 +1,7 @@
+def double(n):
+    return 2 * n
+
+x = double(3)
+print(x)
+
+print("1st modification for 2nd commit")
